be22f73e65b840932eca7ed32968ac30d6c75711
Emily Jorgensen <emjorg10@gmail.com>
2024-03-06 15:44:28 -0600

    brackets index with hard-coded data

--- src/Content.jsx
+++ src/Content.jsx
@@ -1,7 +1,15 @@
+import { BracketsIndex } from "./BracketsIndex";
+
+const brackets = [
+  { id: 1, name: "Perfect Bracket", user: "perfect", total_points: 0 },
+  { id: 2, name: "Alex's Bracket", user: "alex", total_points: 0 },
+  { id: 3, name: "Emily's Bracket", user: "emily", total_points: 0 },
+];
+
 export function Content() {
   return (
-    <main>
-      <h1>Welcome to React!</h1>
-    </main>
+    <div>
+      <BracketsIndex brackets={brackets} />
+    </div>
   );
 }
